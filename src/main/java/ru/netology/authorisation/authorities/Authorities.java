0590cae8866a72b4642aed4c05d43d091a803868
AnhagaBallas <102907803+AnhagaBallas@users.noreply.github.com>
2023-08-12 20:52:43 +0300

    Delete src directory

--- src/main/java/ru/netology/authorisation/authorities/Authorities.java
+++
@@ -1,5 +0,0 @@
-package ru.netology.authorisation.authorities;
-
-public enum Authorities {
-    READ, WRITE, DELETE
-}
--- src/main/java/ru/netology/authorisation/exceptions/UnauthorizedUser.java
+++
@@ -1,7 +0,0 @@
-package ru.netology.authorisation.exceptions;
-
-public class UnauthorizedUser extends RuntimeException {
-    public UnauthorizedUser(String msg) {
-        super(msg);
-    }
-}
